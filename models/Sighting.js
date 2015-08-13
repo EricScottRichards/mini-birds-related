9cea20c32c382ef726e5491e77cdd59302dd64f6
EricScottRichards <ericscottrichards@gmail.com>
2015-08-13 12:49:00 -0600

    done i think

--- models/Sighting.js
+++ models/Sighting.js
@@ -1,24 +1,27 @@
 var mongoose = require('mongoose');
 
 var sightingSchema = new mongoose.Schema({
-  name: { type: String, lowercase: true },
-  order: { type: String, lowercase: true, maxlength: 20 },
-  status: {
-    type: String,
-    lowercase: true,
-    enum: [
-      'extinct',
-      'extinct in the wild',
-      'critically endangered',
-      'endangered',
-      'vulnerable',
-      'near threatened',
-      'conservation dependent',
-      'least concern'
-    ]
-  },
-  confirmed: { type: Boolean, default: false },
-  numberSeen: { type: Number, min: 1 }
+  user       : { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+  bird       : [{
+    name       : { type: String, lowercase: true },
+    order      : { type: String, lowercase: true, maxlength: 20 },
+    status     : {
+      type       : String,
+      lowercase  : true,
+      enum       : [
+          'extinct',
+          'extinct in the wild',
+          'critically endangered',
+          'endangered',
+          'vulnerable',
+          'near threatened',
+          'conservation dependent',
+          'least concern'
+        ]
+    }
+  }],
+  confirmed  : { type: Boolean, default: false },
+  numberSeen : { type: Number, min: 1 }
 });
 
 
